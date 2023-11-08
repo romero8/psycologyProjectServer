2168ecc4f4452067452a63c4188ce366ed78f162
romero8 <101278601+romero8@users.noreply.github.com>
2023-11-08 11:31:26 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -42,7 +42,6 @@ function createToken(id) {
 
 // "https://mangisite.netlify.app"
 
-app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
@@ -50,7 +49,9 @@ app.use(
     credentials: true,
   })
 );
-
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }))
+app.use(express.json());
 let users = [];
 
 mongoose
